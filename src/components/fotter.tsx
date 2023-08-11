fca9581d87f95f41acce9d4901a9e349a011d0f4
unknown <harshal011kamble@gmail.com>
2023-08-11 18:35:06 +0530

    added more final touches

--- src/components/fotter.tsx
+++ src/components/fotter.tsx
@@ -4,17 +4,16 @@ let Fotter: FC = () => {
     return (
         <div id="fotter">
             <div className="fotter-main">
-                <p style={{ fontSize: "1.5rem" }}>I Like bringing like minded people Together!</p>
+                <p style={{ fontSize: "1.2rem" }} className="nicuee">I Like bringing like minded people Together!</p>
                 <br />
-                <p style={{marginBottom:"4px"}}>11.sync.er@gmail.com</p>
-                <p>+91 77109 37556</p>
+                <p style={{marginBottom:"4px",color:"grey"}}>11.sync.er@gmail.com</p>
             </div>
             <div className="socials">
-                <p>Socials</p>
+                <p className="nicuee">Socials</p>
                 <br />
                 <a href="https://twitter.com/harshal_ishere">twitter</a>
                 <a href="https://github.com/harshal-ishere">github</a>
-                <a href="https://www.linkedin.com/in/harshal-k-5b3354287/">linkdin</a>
+                <a href="https://www.linkedin.com/in/harshal-k-5b3354287/">linkedin</a>
             </div>
         </div>
     )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,19 @@
 import { FC } from "react";
 import "./styles.css"
+import {useState} from "react";
+import {GiHamburgerMenu} from "react-icons/gi";
+import {ImCancelCircle} from "react-icons/im";
 let Navbar: FC = () => {
+    let [hamToggle,setHamToggle]=useState<boolean>(false);
     return (
         <nav className="fixed-top">
             <div className="nav-heading">
                 <p>@harshal.ishere</p>
+                <div className={hamToggle?"nav-items-toggle":"invincible"}>
+                <a href="#home">Home</a>
+                <a href="#projects">Projects</a>
+                <a href="#about">About</a>
+            </div>
             </div>
             <div className="nav-items">
                 <a href="#home">Home</a>
@@ -16,6 +25,8 @@ let Navbar: FC = () => {
                 <a href="https://github.com/harshal-ishere" > <img src={require('../img/github.png')} alt="github" className="nav-bar-imgs" /></a>
                 <a href="https://www.linkedin.com/in/harshal-k-5b3354287/" > <img src={require('../img/linkedin.png')} alt="linkedin" className="nav-bar-imgs" /></a>
             </div>
+            
+            {hamToggle?<ImCancelCircle onClick={()=>setHamToggle(!hamToggle)} />:<GiHamburgerMenu className="hamburger" onClick={()=>setHamToggle(!hamToggle)} />}
         </nav>
     )
 }
